da49ae620f90bca67bdd5d27c9411bc70654f472
aldoaquino451 <aldoaquino2909@gmail.com>
2024-01-19 11:42:04 +0100

    [edit] delete modal

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,45 +1,27 @@
 @extends('layouts.admin')
 
 @section('content')
-
-
-
-
-    {{-- <form class="d-inline-block" action="{{ route('admin.products.destroy', $productToDelete) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">
-            Conferma</i>
-        </button>
-    </form> --}}
-
-    <div class="test h-100 w-100"></div>
-
-
     <h2 class="mb-3 text-capitalize text-center">Lista Prodotti</h2>
 
-    @if (!$products)
-        <p class="text-center">Devi creare prima il ristorante!</p>
-    @else
-        <div class="my-4 d-flex justify-content-center">
-            <a class="p-0 btn btn-primary fs-2" href="{{ route('admin.products.create') }}">
-                <div class="py-0 px-2 btn btn-primary d-flex align-items-center gap-3">
-                    <span>Crea un nuovo prodotto</span>
-                    <span class=" fs-2">
-                        <i class="fa-solid fa-circle-plus"></i>
-                    </span>
-                </div>
-            </a>
-        </div>
+    <div class="my-4 d-flex justify-content-center">
+        <a class="p-0 btn btn-primary fs-2" href="{{ route('admin.products.create') }}">
+            <div class="py-0 px-2 btn btn-primary d-flex align-items-center gap-3">
+                <span>Crea un nuovo prodotto</span>
+                <span class=" fs-2">
+                    <i class="fa-solid fa-circle-plus"></i>
+                </span>
+            </div>
+        </a>
+    </div>
 
-        @if ($products->count() > 0)
-            <div class="container d-flex flex-wrap justify-content-center">
-                @foreach ($products as $product)
-                    {{-- <a class="product-card d-inline-block text-decoration-none"
-                        href="{{ route('admin.products.edit', $product) }}"> --}}
-                    <div class="card my-3 mx-3" style="width: 18rem; height: 18rem;">
+    @if ($products->count() > 0)
+        <div class="container d-flex flex-wrap justify-content-center">
+            @foreach ($products as $product)
+                <a class="product-card d-inline-block text-decoration-none"
+                    href="{{ route('admin.products.edit', $product) }}">
+                    <div class="card my-3 mx-3" style="width: 18rem; min-height: 18rem;">
                         {{-- <img src="..." class="card-img-top" alt="..."> --}}
-                        <div class="card-body">
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title">{{ $product->name }}</h5>
                             <h5 class="card-title">&euro;{{ $product->price }}</h5>
                             <p class="card-text">{{ $product->ingredients }}</p>
@@ -49,55 +31,63 @@
                             @if ($product->is_visible === 1)
                                 <p class="mb-2 fw-bold text-warning">E' visibile</p>
                             @endif
-                            <div class="button-position">
-                                <button onclick="show({{ $product }})" type="submit" class="btn btn-danger">
+                            <div class="button-trash d-flex justify-content-end align-items-end">
+                                <button class="btn btn-danger delete-btn" data-toggle="modal"
+                                    data-target="#deleteModal{{ $product->id }}">
                                     <i class="fa-solid fa-trash"></i>
                                 </button>
-
                             </div>
                         </div>
                     </div>
-                    {{-- </a> --}}
-                @endforeach
-            </div>
-        @else
-            <p class=" text-center">Non ci sono prodotti!</p>
-        @endif
+                </a>
+
+                <div class="modal fade" id="deleteModal{{ $product->id }}" tabindex="-1" role="dialog"
+                    aria-labelledby="deleteModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content my-modal-content py-4 px-4">
+                            <div class="d-flex justify-content-center mb-3">
+                                <i class="fa-solid fa-triangle-exclamation fs-1 text-danger"></i>
+                            </div>
+                            <div>
+                                <p class="mb-2 text-center">Sei sicuro di eliminare questo prodotto dal tuo ristorante?
+                                </p>
+                                <p class="text-center fw-bold">{{ $product->name }}</p>
+                            </div>
+                            <div class="d-flex justify-content-center gap-3">
+                                <form action="{{ route('admin.products.destroy', $product) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Conferma</button>
+                                </form>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @else
+        <p class=" text-center">Non ci sono prodotti!</p>
     @endif
 
 @endsection
 
 @section('script')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        const test = document.querySelector(".test");
-        const div = document.createElement("div");
-        div.classList.add('delete-modal-container');
-        test.append(div);
+        document.addEventListener('DOMContentLoaded', function() {
+            // apri il layout della delete modal
+            $('.delete-btn').click(function() {
+                var productId = $(this).data('product-id');
+                $('#deleteModal' + productId).modal('show');
+            });
 
-
-        function show(product) {
-            console.log('ciao');
-            console.log(product);
-            div.classList.add('d-flex');
-            div.innerHTML = `
-            <div class="delete-modal d-flex flex-column align-items-center">
-              <i class="fa-solid fa-triangle-exclamation fs-1 text-danger"></i>
-              <p class="my-3 text-center">
-                Sei sicuro di eliminare questo prodotto dal tuo ristorante?
-              </p>
-              <p class="text-center">product</p>
-              <div class="d-flex gap-3">
-                <button onclick="hide()" class="btn btn-secondary"">
-                  Annulla
-                </button>
-              </div>
-            </div>
-            `;
-        }
-
-        function hide() {
-            div.classList.remove('d-flex')
-        }
+            // previene l'evento che porta all'edit del prodotto
+            var deleteButtons = document.querySelectorAll('.delete-btn');
+            deleteButtons.forEach(function(button) {
+                button.addEventListener('click', function(event) {
+                    event.preventDefault();
+                });
+            });
+        });
     </script>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,6 +17,13 @@
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 
+    <!-- Includi Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
 </head>
 
 <body>
