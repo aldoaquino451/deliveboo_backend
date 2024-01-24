719e8dd749af90115a5c1a7de4104405430c6efd
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-01-24 12:07:06 +0100

    [init] baintree payments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Braintree\Gateway;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +19,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->singleton(Gateway::class, function($app) {
+            return new Gateway(
+                [
+                    'environment' => 'sandbox',
+                    'merchantId' => 'b22n4gtx5t4h7dc7',
+                    'publicKey' => '9c79dxxm47vsbghn',
+                    'privateKey' => '5a154ed3a5b5f55f927a8797d43a346c'
+                ]
+            );
+
+        });
     }
 }
