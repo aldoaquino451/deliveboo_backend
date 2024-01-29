27202b6efaedb8e4b07f2f1a5956060f28a9c313
aldoaquino451 <aldoaquino2909@gmail.com>
2024-01-29 11:56:47 +0100

    [add] restaurant image in seeder

--- database/seeders/RestaurantsSeeder.php
+++ database/seeders/RestaurantsSeeder.php
@@ -9,23 +9,24 @@ use App\Models\Restaurant;
 
 class RestaurantsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        $restaurants = config('restaurants');
-        foreach ($restaurants as $restaurant) {
-        $new_restaurant = new Restaurant();
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $restaurants = config('restaurants');
+    foreach ($restaurants as $restaurant) {
+      $new_restaurant = new Restaurant();
 
-        $new_restaurant->user_id = $restaurant['user_id'];
-        $new_restaurant->name_restaurant = $restaurant['name_restaurant'];
-        $new_restaurant->slug = Helper::generateSlug($new_restaurant->name_restaurant, Restaurant::class);
-        $new_restaurant->address = $restaurant['address'];
-        $new_restaurant->vat_number = $restaurant['vat_number'];
-        $new_restaurant->description = $restaurant['description'];
+      $new_restaurant->user_id = $restaurant['user_id'];
+      $new_restaurant->name_restaurant = $restaurant['name_restaurant'];
+      $new_restaurant->slug = Helper::generateSlug($new_restaurant->name_restaurant, Restaurant::class);
+      $new_restaurant->address = $restaurant['address'];
+      $new_restaurant->vat_number = $restaurant['vat_number'];
+      $new_restaurant->image = $restaurant['image'];
+      $new_restaurant->description = $restaurant['description'];
 
-        $new_restaurant->save();
-        }
+      $new_restaurant->save();
     }
+  }
 }
