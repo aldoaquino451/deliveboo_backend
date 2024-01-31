154c03460500e7aeed0f5c84008819c1c9bb1362
aldoaquino451 <aldoaquino2909@gmail.com>
2024-01-31 15:59:20 +0100

    [edit] aside mobile

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -2,17 +2,25 @@
     <ul>
         <li>
             <a href="{{ route('admin.products.index') }}"
-                class="{{ Route::is('admin.products.index') ? 'active' : '' }} link-light "><i
-                    class="fa-solid fa-list-ul"></i><span>Prodotti</span></a>
+                class="{{ Route::is('admin.products.index') ? 'active' : '' }} link-light ">
+                <i class="fa-solid fa-list-ul"></i><span>Prodotti</span>
+            </a>
         </li>
         <li>
             <a href="{{ route('admin.dashboard') }}"
-                class="{{ Route::is('admin.dashboard') ? 'active' : '' }} link-light "><i
-                    class="fa-solid fa-square-poll-vertical"></i><span>Statistiche</span></a>
+                class="{{ Route::is('admin.dashboard') ? 'active' : '' }} link-light ">
+                <i class="fa-solid fa-square-poll-vertical"></i><span>Statistiche</span>
+            </a>
         </li>
         <li>
             <a href="{{ route('admin.orders.index') }}"
-                class="{{ Route::is('admin.orders.index') ? 'active' : '' }} link-light "><i class="fa-solid fa-clipboard-list"></i><span>Ordini</span></a>
+                class="{{ Route::is('admin.orders.index') ? 'active' : '' }} link-light ">
+                <i class="fa-solid fa-clipboard-list"></i><span>Ordini</span>
+            </a>
         </li>
     </ul>
 </aside>
+
+<div class="btn-aside">
+    <button class="btn btn-danger">ciao</button>
+</div>
