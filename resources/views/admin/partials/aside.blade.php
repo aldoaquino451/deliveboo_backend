0ae18a3df68a3f64ee58efb61657c06d6329c3c6
aldoaquino451 <aldoaquino2909@gmail.com>
2024-01-31 18:01:28 +0100

    [add] button aside

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -7,20 +7,21 @@
             </a>
         </li>
         <li>
-            <a href="{{ route('admin.dashboard') }}"
-                class="{{ Route::is('admin.dashboard') ? 'active' : '' }} link-light ">
-                <i class="fa-solid fa-square-poll-vertical"></i><span>Statistiche</span>
-            </a>
-        </li>
-        <li>
             <a href="{{ route('admin.orders.index') }}"
                 class="{{ Route::is('admin.orders.index') ? 'active' : '' }} link-light ">
                 <i class="fa-solid fa-clipboard-list"></i><span>Ordini</span>
             </a>
         </li>
+        <li>
+            <a href="{{ route('admin.dashboard') }}"
+                class="{{ Route::is('admin.dashboard') ? 'active' : '' }} link-light ">
+                <i class="fa-solid fa-square-poll-vertical"></i><span>Statistiche</span>
+            </a>
+        </li>
+
     </ul>
 </aside>
 
-<div class="btn-aside">
+{{-- <div class="btn-aside">
     <button class="btn btn-danger">ciao</button>
-</div>
+</div> --}}
--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -2,50 +2,56 @@
 
 @section('content')
     <div class="container">
-    <h1 class="mb-3 text-center logo">{{$restaurant->name_restaurant}}</h1>
+        <h1 class="mb-4 mt-3 text-center logo">{{ $restaurant->name_restaurant }}</h1>
 
-    <div class="px-md-5 d-flex flex-column flex-md-row justify-content-center align-items-center">
+        <div style="max-width: 1000px" class="px-0 px-xl-5 mx-auto d-flex flex-column flex-lg-row justify-content-center">
 
-        <div>
-            <p class="mb-2">
-                <strong>Indirizzo: </strong>
-                <span class="ms-2">{{ $restaurant->address }}</span>
-            </p>
-            <p class="mb-2">
-                <strong>Descrizione: </strong>
-                <span class="ms-2">{{ $restaurant->description }}</span>
-            </p>
-            <p class="mb-2">
-                <strong>Email:</strong>
-                <span class="ms-2">{{ $restaurant->user->email }}</span>
-            </p>
-            <p class="mb-2">
-                <strong>Partita IVA: </strong>
-                <span class="ms-2">{{ $restaurant->vat_number }}</span>
-            </p>
-            <p class="mb-2">
-                <strong>Tipologie: </strong>
-                @foreach ($restaurant->typologies as $typology)
-                    <span style="background-color: #a73922" class="badge bg-cst" class="ms-2">{{ $typology->name }}</span>
-                @endforeach
-            </p>
+            <div class="restaurant-details container-fluid">
+                <div class="row d-flex mb-2">
+                    <strong class="col-4  col-lg-3 text-end">Indirizzo: </strong>
+                    <span class="col-8 ">{{ $restaurant->address }}</span>
+                </div>
+                <div class="row d-flex mb-2">
+                    <strong class="col-4  col-lg-3 text-end">Descrizione: </strong>
+                    <span class="col-8 ">{{ $restaurant->description }}</span>
+                </div>
+                <div class="row d-flex mb-2">
+                    <strong class="col-4  col-lg-3 text-end">Email:</strong>
+                    <span class="col-8 ">{{ $restaurant->user->email }}</span>
+                </div>
+                <div class="row d-flex mb-2">
+                    <strong class="col-4  col-lg-3 text-end">Partita IVA: </strong>
+                    <span class="col-8 ">{{ $restaurant->vat_number }}</span>
+                </div>
+
+                <div class="row d-flex mb-2">
+                    <strong class="col-4  col-lg-3 text-end">Tipologie: </strong>
+                    <div class="col-8 ">
+                        @foreach ($restaurant->typologies as $typology)
+                            <span style="background-color: #a73922" class="badge bg-cst"
+                                class="ms-2">{{ $typology->name }}</span>
+                        @endforeach
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="restaurant-image">
+                <img style="width: 100%; border-radius: 20px" src="{{ $restaurant->image }}"
+                    alt="{{ $restaurant->image_original_name }}">
+            </div>
         </div>
 
-        <div style="max-width: 300px" class="ms-md-5">
-            <img style="width: 100%" src="{{ $restaurant->image }}" alt="{{ $restaurant->image_original_name }}">
-        </div>
-    </div>
 
-
-        <table style="max-width: 1000px" class="table table-striped table-hover mt-3 mx-auto">
+        <table style="max-width: 1000px" class="table table-striped table-hover mt-3 mx-auto shadow rounded">
             <thead>
                 <tr class="text-center">
-                    <th scope="col">Numero Ordine</th>
-                    <th scope="col">Cliente</th>
-                    <th scope="col" colspan="2">Indirizzo di consegna</th>
-                    <th scope="col">Data</th>
-                    <th scope="col">Spesa Totale</th>
-                    <th scope="col">Azioni</th>
+                    <th scope="col" class="col-5 col-md-2">Numero Ordine</th>
+                    <th scope="col" class="d-none d-md-table-cell col-md-2">Cliente</th>
+                    <th scope="col" colspan="2" class="d-none d-md-table-cell col-md-2">Indirizzo di consegna</th>
+                    <th scope="col" class="col-5 col-md-2">Data</th>
+                    <th scope="col" class="d-none d-md-table-cell col-md-2">Spesa Totale</th>
+                    <th scope="col" class="col-2 col-md-2">Azioni</th>
 
                 </tr>
             </thead>
@@ -54,13 +60,13 @@
                 @foreach ($orders as $order)
                     <tr class="text-center">
                         <th>#{{ $order->order_number }}</th>
-                        <td>{{ $order->name }} {{ $order->lastname }}</td>
-                        <td colspan="2">{{ $order->address }}</td>
+                        <td class="d-none d-md-table-cell">{{ $order->name }} {{ $order->lastname }}</td>
+                        <td class="d-none d-md-table-cell" colspan="2">{{ $order->address }}</td>
                         <td>{{ $order->formatted_created_at }}</td>
-                        <td>&euro; {{ $order->total_price }}</td>
+                        <td class="d-none d-md-table-cell">&euro; {{ $order->total_price }}</td>
                         <td>
-                            <a href="{{ route('admin.orders.show', $order ) }}" class="card-link btn btn-secondary d-inline-block"><i
-                                    class="fa-regular fa-eye"></i></a>
+                            <a href="{{ route('admin.orders.show', $order) }}"
+                                class="card-link btn btn-secondary d-inline-block"><i class="fa-regular fa-eye"></i></a>
                         </td>
 
                     </tr>
@@ -69,5 +75,4 @@
         </table>
 
     </div>
-
 @endsection
