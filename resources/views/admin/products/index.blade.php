2c2217dba43fc2cd23564c5b3379616f78bdf841
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-01-31 12:15:56 +0100

    [fix] responsive

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -15,7 +15,7 @@
 
     @if ($products->count() > 0)
         <div class="container d-flex flex-wrap justify-content-center">
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
 
                 @foreach ($products as $product)
                     <div class="col">
--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.admin')
 
 @section('content')
+    <div class="container">
     <h1 class="mb-3 text-center logo">{{$restaurant->name_restaurant}}</h1>
 
-    <div class="px-5 d-flex justify-content-center align-items-center">
-        
+    <div class="px-md-5 d-flex flex-column flex-md-row justify-content-center align-items-center">
+
         <div>
             <p class="mb-2">
                 <strong>Indirizzo: </strong>
@@ -30,40 +31,43 @@
             </p>
         </div>
 
-        <div style="max-width: 300px" class="ms-5 p-3">
+        <div style="max-width: 300px" class="ms-md-5">
             <img style="width: 100%" src="{{ $restaurant->image }}" alt="{{ $restaurant->image_original_name }}">
         </div>
     </div>
 
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr class="text-center">
-                <th scope="col">Numero Ordine</th>
-                <th scope="col">Cliente</th>
-                <th scope="col" colspan="2">Indirizzo di consegna</th>
-                <th scope="col">Data</th>
-                <th scope="col">Spesa Totale</th>
-                <th scope="col">Azioni</th>
 
-            </tr>
-        </thead>
-        <tbody>
-            </tr>
-            @foreach ($orders as $order)
+        <table style="max-width: 1000px" class="table table-striped table-hover mt-3 mx-auto">
+            <thead>
                 <tr class="text-center">
-                    <th>#{{ $order->order_number }}</th>
-                    <td>{{ $order->name }} {{ $order->lastname }}</td>
-                    <td colspan="2">{{ $order->address }}</td>
-                    <td>{{ $order->formatted_created_at }}</td>
-                    <td>&euro; {{ $order->total_price }}</td>
-                    <td>
-                        <a href="{{ route('admin.orders.show', $order ) }}" class="card-link btn btn-secondary d-inline-block"><i
-                                class="fa-regular fa-eye"></i></a>
-                    </td>
+                    <th scope="col">Numero Ordine</th>
+                    <th scope="col">Cliente</th>
+                    <th scope="col" colspan="2">Indirizzo di consegna</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Spesa Totale</th>
+                    <th scope="col">Azioni</th>
 
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                </tr>
+                @foreach ($orders as $order)
+                    <tr class="text-center">
+                        <th>#{{ $order->order_number }}</th>
+                        <td>{{ $order->name }} {{ $order->lastname }}</td>
+                        <td colspan="2">{{ $order->address }}</td>
+                        <td>{{ $order->formatted_created_at }}</td>
+                        <td>&euro; {{ $order->total_price }}</td>
+                        <td>
+                            <a href="{{ route('admin.orders.show', $order ) }}" class="card-link btn btn-secondary d-inline-block"><i
+                                    class="fa-regular fa-eye"></i></a>
+                        </td>
+
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
 
 @endsection
