e954cab4706c548b532450e77590e1272bd38ffc
aldoaquino451 <aldoaquino2909@gmail.com>
2024-02-01 01:35:17 +0100

    [edit] show

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,50 +1,50 @@
 @extends('layouts.admin')
 
 @section('content')
+    <div class="container-fluid container-lg" style="max-width: 800px ">
 
-    <div class="container-fluid">
-        <div class="row row-cols-lg-2">
-            <div class="col col-lg-8 py-3 px-4">
-                <h3 class="mb-4">Dettaglio ordine</h3>
+        <h2 class="text-center fw-bold py-4">Ordine: # {{ $order->order_number }}</h2>
 
-                <table class="table table-striped table-hover">
-                    <thead>
-                        <tr class="text-center">
-                            <th scope="col">Quantità</th>
-                            <th scope="col">Prodotto</th>
-                            <th scope="col">Prezzo</th>
-                            <th scope="col">Totale</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($products as $product)
-                        <tr class="text-center">
-                            <td>{{ $product->pivot->quantity }}</td>
-                            <td>{{ $product->name }}</td>
-                            <td>&euro; {{ $product->price }}</td>
-                            <td>&euro; {{ number_format($product->pivot->quantity * $product->price, 2) }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                    <tfoot>
-                        <th></th>
-                        <th></th>
-                        <th class="text-end">Totale:</th>
-                        <th class="text-center">&euro; {{$order->total_price}}</th>
-                    </tfoot>
-                </table>
-            </div>
+        <div class="d-flex mb-5">
+            <h4 class="mb-4 w-25 fw-bold ">Consegna:</h4>
+            <div class="w-75">
+                <p class="mb-2 fw-light">Nome: <strong class="ms-3 text-capitalize fw-bold">{{ $order->name }}
+                        {{ $order->lastname }}</strong></p>
+                <p class="mb-2 fw-light">Indirizzo: <strong
+                        class="ms-3 text-capitalize fw-bold">{{ $order->address }}</strong></p>
+                <p class="mb-2 fw-light">Email: <strong class="ms-3 fw-bold">{{ $order->email }}</strong></p>
+                <p class="mb-2 fw-light">Telefono: <strong class="ms-3 fw-bold">{{ $order->phone_number }}</strong></p>
 
-            <div class="col col-lg-4 py-3 px-4">
-                <h3 class="mb-4">Dettagli consegna:</h3>
-                <p>Nome: <strong class="ms-3">{{$order->name}}</strong></p>
-                <p>Cognome: <strong class="ms-3">{{$order->lastname}}</strong></p>
-                <p>Indirizzo: <strong class="ms-3">{{$order->address}}</strong></p>
-                <p>Email: <strong class="ms-3">{{$order->email}}</strong></p>
-                <p>Telefono: <strong class="ms-3">{{$order->phone_number}}</strong></p>
             </div>
         </div>
 
-    </div>
+        <div class="d-flex">
+            <h4 class="mb-4 w-25 fw-bold ">Prodotti:</h4>
 
+            <table class="table table-striped table-hover w-75">
+                <thead>
+                    <tr class="text-center">
+                        <th scope="col">Prodotto</th>
+                        <th scope="col">Quantità</th>
+                        <th scope="col">Prezzo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr class="text-center">
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->pivot->quantity }}</td>
+                            <td>&euro; {{ $product->price }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+                <tfoot>
+                    <th></th>
+                    <th class="text-end">Totale:</th>
+                    <th class="text-center">&euro; {{ $order->total_price }}</th>
+                </tfoot>
+            </table>
+        </div>
+
+    </div>
 @endsection
